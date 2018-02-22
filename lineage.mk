25ded9a0bb8fedeb50f8d7aa35c02d40f794be7d
NefIntent <nefintent@zeroside.co>
2018-02-22 09:23:58 -0800

    prague: LineageOS-ify

---
+++ lineage.mk
@@ -0,0 +1,10 @@
+# Device
+$(call inherit-product, device/huawei/prague/full_prague.mk)
+
+# Inherit common Lineage phone.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := lineage_prague
+PRODUCT_DEVICE := prague
+
--- omni_prague.mk
+++
@@ -1,8 +0,0 @@
-# Device
-$(call inherit-product, device/huawei/prague/full_prague.mk)
-
-# OmniROM 7.1 Nougat
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
-PRODUCT_NAME := omni_prague
