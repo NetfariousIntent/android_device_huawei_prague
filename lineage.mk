3e8503276900ae4b5898c332e81276a1f2660574
NefIntent <nefintent@zeroside.co>
2018-02-22 09:39:12 -0800

    prague: We still have CM

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/huawei/prague/full_prague.mk)
 
 # Inherit common Lineage phone.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := lineage_prague
